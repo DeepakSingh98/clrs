1c10a352cf6a978f0e01ffb939d971c6db9a35df
Deepak Singh <deepak@Deepaks-MacBook-Pro.local>
2024-04-22 17:25:07 -0400

    try reshape

--- clrs/_src/nets.py
+++ clrs/_src/nets.py
@@ -484,7 +484,8 @@ class Net(hk.Module):
         if dp.type_ == _Type.POINTER:
             # Reverse the input
             reversed_data = jnp.transpose(dp.data, axes=(1, 0))  # Swap dimensions for reversed pointers
-            reversed_data = reversed_data[:, :, None]  # Add an extra dimension for feature_dim
+            reversed_data = reversed_data[:, :, None, None]  # Add an extra dimension for feature_dim
+            reversed_data = jnp.tile(reversed_data, (1, 1, dp.data.shape[0], 1))
             reversed_dp = probing.DataPoint(
                 name=dp.name + '_reversed',
                 location=_Location.EDGE,
